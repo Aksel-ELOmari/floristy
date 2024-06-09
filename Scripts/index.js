cde318614c407cee1c2629f0cda82617ab6039c4
Aksel-ELOmari <129563644+Aksel-ELOmari@users.noreply.github.com>
2024-06-09 20:48:27 +0100

    Add files via upload

---
+++ Scripts/index.js
@@ -0,0 +1,7 @@
+const exbtn = document.querySelector('.ex-btn');
+exbtn.onclick = function(){
+    document.querySelector('.nav-links').style.display = 'none';
+}
+
+
+// const footerask = documnet.querySelector('.other-links');
